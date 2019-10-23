bbbfb1dbb752708a237155f0089290be874be841
rebecca.bieber <rebecca.bieber@gmail.com>
2019-10-22 20:37:15 -0400

    loop practice

--- RB/loop practice.R
+++ RB/loop practice.R
@@ -1,1 +1,16 @@
 #Loop practice!
+
+for(this_number in 1:10) {
+    cat("Here's number", this_number, "\n")
+    
+}
+
+
+for(this_number in 1:10000) {
+    if(this_number %% 1000 == 0) {cat("starting number", this_number, "\n" )
+    }
+    result = this_number * 3
+}
+
+#modulo is integer division
+
