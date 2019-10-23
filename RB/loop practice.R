e63d676cea89e11d1e7c03c2b00d5a9683ac798b
rebecca.bieber <rebecca.bieber@gmail.com>
2019-10-22 20:57:25 -0400

    practice

--- RB/loop practice.R
+++ RB/loop practice.R
@@ -14,3 +14,33 @@ for(this_number in 1:10000) {
 
 #modulo is integer division
 
+
+mylist = list(0, "cheese", TRUE)
+
+for(this_thing in mylist) {
+    cat(this_thing)
+}
+
+for(this_thing in mylist) {
+    if(is.logical (this_thing)) {
+        cat("this is logical\n")
+    } else{
+        cat("this is not logical \n")
+    }
+}
+
+
+
+for(this_thing in mylist) {
+    if(is.logical (this_thing)) {
+        cat("this is logical\n")
+    } else if (is.numeric(this_thing)) {
+        cat("this is not logical, but it's a number \n")
+    } else {
+        cat("this is not very logical \n")
+    }
+        
+}
+
+
+
